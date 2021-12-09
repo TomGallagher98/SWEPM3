50ef2736ff0de96357123a027ccd026bd56063ef
Maximilian Mandel <79708659+git-maximilianm@users.noreply.github.com>
2021-12-09 01:21:22 +0100

    Add files via upload

---
+++ moodle/moodle-composition/volume/app/mod/version.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2021120800;
+$plugin->requires = 2020110903;
+$plugin->component = 'mod_learningactivity';
+$plugin->release = "v3.10-r1"
+
+$plugin->dependencies = [
+    //add dependencies here
+];
